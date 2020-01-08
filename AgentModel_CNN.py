2793a66af67c8023d26715d1aee545ab1b542028
mqshao <s1752729916@qq.com>
2020-01-08 18:49:40 +0800

    1

--- AgentModel_CNN.py
+++ AgentModel_CNN.py
@@ -1,4 +1,22 @@
 import torch.nn as nn
 import torchvision
-print(444)
-print(666)
+import pandas as pd
+import numpy as np
+class DataProcess:
+
+    def PreProcess(self):
+        #数据预处理，读取数据并且将数据拼成CNN的图片模式
+        raw = pd.read_csv('abaqus_12_8.csv')
+        self.rawdata = raw[:,0:16]
+        self.labels = raw[:,17:]
+
+
+a = DataProcess()
+a.PreProcess()
+print(a.rawdata[0])
+print(a.labels[0])
+
+
+
+
+
