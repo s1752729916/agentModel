1379c3aaf3c82a94feaa2d2746883f383e69d6c0
mqshao <s1752729916@qq.com>
2020-01-08 18:52:13 +0800

    1

--- AgentModel_CNN.py
+++ AgentModel_CNN.py
@@ -6,7 +6,8 @@ class DataProcess:
 
     def PreProcess(self):
         #数据预处理，读取数据并且将数据拼成CNN的图片模式
-        raw = pd.read_csv('abaqus_12_8.csv')
+        raw = pd.read_csv('abaqus_12_8_plastic.csv')
+        raw = raw.values
         self.rawdata = raw[:,0:16]
         self.labels = raw[:,17:]
 
